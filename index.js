abf53da05df9da588c925c8b083d4edda7afd245
shannon <shannonf.reidy@gmail.com>
2020-08-17 21:46:39 -0500

    testing bot push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 const bot = new Discord.Client();
-const token = 'NzQ1MTAyNDY0OTQ0MTc3MjUz.Xzs4vg.30EV0tN1jEwIdGTiEQqU0-cuTBE'
+const token = 'NzQ1MTAyNDY0OTQ0MTc3MjUz.Xzs4vg.U6FiSyZNQrGi2TeTjO5nqckIVl8'
 
 bot.on('ready', () => {
   console.log('bot is ready')
