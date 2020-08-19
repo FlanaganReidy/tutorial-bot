8b78b8fc2bfcf08ad50f27bcc41a1cb39879f9f8
shannon <shannonf.reidy@gmail.com>
2020-08-18 20:08:15 -0500

    moved token to untracked file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
+const token = require('./token.js');
 
 const bot = new Discord.Client();
-const token = 'NzQ1MTAyNDY0OTQ0MTc3MjUz.Xzs4vg.U6FiSyZNQrGi2TeTjO5nqckIVl8'
 
 bot.on('ready', () => {
   console.log('bot is ready')
